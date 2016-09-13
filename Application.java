a8de1b8b9c6c19ffe7ad26645da3e1a44879975d
mayur9099 <mayur.citm@gmail.com>
2016-09-12 20:03:57 -0700

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,14 @@
+package com.apple.siri.audioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Created by blindblom on 4/6/16.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args);
+  }
+}
